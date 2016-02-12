39e988ab892ae58b8eacb090e4e6872ce0459379
name <mwharris1313@gmail.com>
2016-02-11 23:45:20 -0500

    Add Gin Framework

--- main.go
+++ main.go
@@ -3,11 +3,15 @@ package main
 import (
   "net/http"
   "github.com/mwharris1313/goapi/api"
+  "github.com/gin-gonic/gin"
 )
 
 func main()  {
+  r := gin.Default()
+
   http.HandleFunc(api.RootPath, api.RootHandler)
   http.HandleFunc(api.AboutPath, api.AboutHandler)
 
   http.ListenAndServe(":3000", nil)
+  r.Run(":3000")
 }
