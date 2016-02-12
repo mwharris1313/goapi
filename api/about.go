bf1bef2480534b8b09a40985e7e919ada8f5d1fd
name <mwharris1313@gmail.com>
2016-02-11 23:44:46 -0500

    Add marshalling

--- api/about.go
+++ api/about.go
@@ -6,18 +6,32 @@ import (
 )
 
 type Message struct{
-  Text string
+  Message string `json:"msg"`
+}
+
+type Wrapper struct{
+  WMessage string `json:"wmsg"`
+  Data Message `json:"data"`
 }
 
 var AboutPath = "/about/"
 
 func AboutHandler(w http.ResponseWriter, r *http.Request)  {
   msg := Message{"goapi API v0a (alpha)"}
-  jsonMsg, err := json.Marshal(msg)
+  wrap := Wrapper{
+    WMessage: "WrapperMessage",
+    Data: msg,
+  }
+  // jsonMsg, err := json.Marshal(msg)
+  //
+  // if err != nil {
+  //   panic(err)
+  // }
 
+  jsonWrap, err := json.Marshal(wrap)
   if err != nil {
     panic(err)
   }
 
-  w.Write(jsonMsg)
+  w.Write(jsonWrap)
 }
